498894727fc4641ecb62ab0248607ae1d02b74bc
rahulkhn <43348188+rahulkhn@users.noreply.github.com>
2021-03-23 16:47:42 +0000

    Create convert_API.php

---
+++ convert_API.php
@@ -0,0 +1,25 @@
+?php
+
+$curl = curl_init();
+
+curl_setopt_array( $curl, array(
+  CURLOPT_PORT => "443",
+  CURLOPT_URL => "https://marketdata.tradermade.com/api/v1/convert?from=EUR&to=USD&amount=1000&api_key=api_key",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
